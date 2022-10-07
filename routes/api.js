95ef0d17d48d57baffa0d2ee5397552f87ecb4f6
ReTims <renatatims@gmail.com>
2022-10-07 13:21:33 -0500

    Created function writeToFile + app.post to push the notes to the Array and write the note on the left panel

--- routes/api.js
+++ routes/api.js
@@ -2,8 +2,23 @@ const fs = require ("fs");
 const notesArray = require("../db/db.json");
 
 function noteTaker(app){
+    // Function to send the user's notes to the Array located in db.json file.
+    function writeToFile(notes) {
+        notes = JSON.stringify(notes)
+        console.log(notes);
+        fs.writeFile ("./db/db.json", notes, (err) => {
+            err ? console.log(err) : console.log(notes);
+        })
+    }
+    // Get the user's note from the Array and display on the notes path (api/notes)
     app.get("/api/notes", function(req, res){
         res.json(notesArray);
+    });
+    //Post - user's note is pushed to the array and displayed in the left panel
+    app.post ("/api/notes", function (req,res){
+        notesArray.push (req.body);
+        writeToFile (notesArray);
+        res.json (req.body);
     });
 }
 
