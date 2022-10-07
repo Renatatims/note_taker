e1356eaf335e7960bcff85998c9507d6d051bd75
ReTims <renatatims@gmail.com>
2022-10-07 14:14:20 -0500

    Included an if statement to create an unique ID to the user's note and display on the right panel

--- routes/api.js
+++ routes/api.js
@@ -16,6 +16,13 @@ function noteTaker(app){
     });
     //Post - user's note is pushed to the array and displayed in the left panel
     app.post ("/api/notes", function (req,res){
+        //if statement to create a unique ID to the user's note and it will be shown on the right panel
+        if (notesArray.length === 0){
+            req.body.id = "0"; 
+        } else{
+            req.body.id = JSON.stringify(JSON.parse(notesArray[notesArray.length - 1].id) + 1);
+           
+        }
         notesArray.push (req.body);
         writeToFile (notesArray);
         res.json (req.body);
